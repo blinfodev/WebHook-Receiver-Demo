c65df6b39375187c76f4bdb51c2c977e816b028d
Austin Brown <abrown@blinfo.com>
2021-08-18 12:52:09 -0400

    Convert webhook model to camelCase

--- Models/WebHookEvent.model.cs
+++ Models/WebHookEvent.model.cs
@@ -7,37 +7,37 @@ namespace WebHookReceiver.Models
 		/// <summary>
         /// The ID of the User that caused this webhook to occur.
 		/// </summary>
-        public string UserID { get; set; } = string.Empty;
+        public string userID { get; set; } = string.Empty;
 
 		/// <summary>
         /// The company in which this webhook occurred.
 		/// </summary>
-		public string CompanyID { get; set; } = string.Empty;
+		public string companyID { get; set; } = string.Empty;
 
 		/// <summary>
         /// The source of the webhook. This is typically a database table name,
 		/// may also refer to other sources in Odyssey, such as a production transaction.
 		/// </summary>
-		public string Source { get; set; } = string.Empty;
+		public string source { get; set; } = string.Empty;
 
 		/// <summary>
         /// The action that was performed to cause this webhook. Example values are:
 		/// 'ADD' to denote a new record, 'CHANGE' to denote an update, and 'DELETE' to denote record removal.
 		/// </summary>
-		public string Action { get; set; } = string.Empty;
+		public string action { get; set; } = string.Empty;
 
 		/// <summary>
         /// An array of objects. If applicable, this array contains all the records that existed
 		/// prior to the change. For example, if a Product is being updated, this would contain a
 		/// single object that represents the Product before the change.
 		/// </summary>
-		public List<dynamic> PreviousData { get; set; } = null;
+		public List<dynamic> previousData { get; set; } = null;
 
 		/// <summary>
         /// An array of objects. If applicable, this array contains all the records that existed
 		/// after the change. For example, if a Product is being updated, this would contain a
 		/// single object that represents the Product after the change.
 		/// </summary>
-		public List<dynamic> Data { get; set; } = null;
+		public List<dynamic> data { get; set; } = null;
     }
 }
